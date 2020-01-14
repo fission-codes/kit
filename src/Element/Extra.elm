d95997c976e3117f387fd8db5d05b8f00533472a
Steven Vandevelde <icid.asset@gmail.com>
2020-01-14 18:46:04 +0100

    Extra functions for working with Elements

---
+++ src/Element/Extra.elm
@@ -0,0 +1,28 @@
+module Element.Extra exposing (..)
+
+import Element
+import Html
+import Html.Attributes
+
+
+{-| CSS styling for Elements.
+-}
+customStyle : String -> String -> Element.Attribute msg
+customStyle k v =
+    Element.htmlAttribute (Html.Attributes.style k v)
+
+
+{-| `id` attribute.
+-}
+id : String -> Element.Attribute msg
+id =
+    Html.Attributes.id >> Element.htmlAttribute
+
+
+{-| `Element.text` ignores line breaks, this does not.
+-}
+textWithLineBreaks : String -> Element.Element msg
+textWithLineBreaks text =
+    Element.el
+        [ customStyle "white-space" "break-spaces" ]
+        (Element.html <| Html.text text)
